2feef49584ed29c871d4905dced9a6cf24a1feec
Li Jie <lijie@applean.cn>
2016-09-08 20:42:26 +0800

    Add url props to simplify usage, let cable and url optional

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 var React = require('react')
+var ActionCable = require('actioncable')
 const { Component, PropTypes } = React
 
 class ActionCableProvider extends Component {
   getChildContext () {
     return {
-      cable: this.props.cable
+      cable: this.cable
     }
+  }
+
+  componentWillMount () {
+    if (this.props.cable) {
+      this.cable = this.props.cable
+    } else {
+      this.cable = ActionCable.createConsumer(this.props.url)
+    }
+  }
+
+  componentWillUnmount () {
+    if (!this.props.cable && this.cable) {
+      this.cable.disconnect()
+    }
+  }
+
+  componentWillReceiveProps (nextProps) {
+    // Props not changed
+    if (this.props.cable === nextProps.cable &&
+        this.props.url === nextProps.url) {
+      return
+    }
+
+    // cable is created by self, disconnect it
+    this.componentWillUnmount()
+
+    // create or assign cable
+    this.componentWillMount()
   }
 
   render () {
@@ -14,7 +43,8 @@ class ActionCableProvider extends Component {
 }
 
 ActionCableProvider.propTypes = {
-  cable: PropTypes.object.isRequired,
+  cable: PropTypes.object,
+  url: PropTypes.string,
   children: PropTypes.any
 }
 
